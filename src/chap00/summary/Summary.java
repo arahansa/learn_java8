1b7f143be6da9492a35e519af6307e10724f60ee
jarvis <arahansa@naver.com>
2015-10-12 08:34:35 +0900

    learn chap05 and etc

---
+++ src/chap00/summary/Summary.java
@@ -0,0 +1,17 @@
+package chap00.summary;
+
+public class Summary {
+	
+	
+	
+	// 03장 람다표현식. 100페이지 대표적인 함수형 인터페이스 보기
+	
+	// Comparator : 비교 compare
+	// Consumer : 
+	// Predicate : test
+	// Function : andThen
+	
+	
+	// 04 중간 연산 144 페이지 정리된 것 보기
+
+}
