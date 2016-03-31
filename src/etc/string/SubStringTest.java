2c16a34b65c34b81b8e2624f2328b0c48264f785
ararhansa <arahansa@naver.com>
2016-03-31 10:24:33 +0900

    filtering test

--- src/etc/string/SubStringTest.java
+++ src/etc/string/SubStringTest.java
@@ -5,5 +5,8 @@ public class SubStringTest {
 	public static void main(String[] args) {
 		String url ="db_test";
 		System.out.println(url.substring(3));
+		
+		String test="";
+		System.out.println(" substirng for '' "+test.substring(5));
 	}
 }
