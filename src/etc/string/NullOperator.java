48553df558887ee6b860f419c66a37532bac5942
ararhansa <arahansa@naver.com>
2016-04-07 18:57:15 +0900

    add Null operator

--- src/etc/string/NullOperator.java
+++ src/etc/string/NullOperator.java
@@ -4,5 +4,7 @@ public class NullOperator {
 
 	public static void main(String[] args) {
 		System.out.println("널 오퍼레이터 " + ("" + null));
+		System.out.println("널끼리 합치기 되나요? "+ null+null);
+		
 	}
 }
